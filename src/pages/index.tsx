93e99eb4984cfd8dc23e169b2d965ba8e9f83d42
mrxonex <mikhail.poleshchenkov@mail.ru>
2022-09-06 23:21:49 +0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,24 @@
 import { ReactNode, useEffect, useState } from "react";
 import type { NextPage } from "next";
-import Image from "next/image";
+import Image from "next/image"
+import { useRouter } from "next/router";
+//
+import $api from "config";
 // layouts
 import PageContainer from "../layouts/PageContainer";
 // components
 import FilterByQuestions from "../components/FilterByQuestions";
 import SearchByQuestions from "../components/SearchByQuestions";
 import QuestionItem from "../components/QuestionItem";
-//
-import loading from "../utils/gift/loading.gif";
-import $api from "config";
+// components/UI
 import Button from "components/UI/Button";
+// components/Skeletons
 import QuetionItemSkeleton from "components/Skeletons/QuestionItemSkeleton";
+// utils/gift
+import loading from "../utils/gift/loading.gif";
+
+// utils/svg
 import Close from "../utils/svg/remove_xmark.svg";
-import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
   const [questions, setQuestions] = useState<any>(undefined);
@@ -39,32 +44,15 @@ const Home: NextPage = () => {
 
   const router = useRouter();
 
-  const getDonate = () => {
-    if (typeof sessionStorage !== "undefined" && sessionStorage.getItem('donate') !== 'false') {
-      return (
-        <div className="mt-[20px] mx-auto max-w-[330px] relative">
-          <Button
-            onClick={() => sessionStorage.setItem("donate", "false")}
-            className="absolute py-[2px] hover:bg-[rgba(255,255,255,0.4)] px-[4px] rounded-[5px] bg-[rgba(255,255,255,0.25)] right-[5px] top-[5px]"
-          >
-            <Close fill="white" width={10} />
-          </Button>
-          <Button
-            onClick={() =>
-              router.push(
-                "https://boosty.to/youq/single-payment/donation/216935"
-              )
-            }
-            className="bg-[rgba(171,160,27,0.8491771708683473)] rounded-[10px] py-[7px] w-full text-white font-nunito font-bold text-[17px]"
-          >
-            Поддержать проект
-          </Button>
-        </div>
-      );
-    }
 
-    return <></>
-  };
+  const getQuestions = async () => {
+    const res = await $api.get(`question/getAll?limit=${questions.length + 5 ?? 5}`);
+
+    if (res.status === 200) {
+      setQuestions(res.data);
+    }
+  }
+
 
   return (
     <PageContainer>
@@ -97,7 +85,7 @@ const Home: NextPage = () => {
                     <QuestionItem key={question.id} question={question} />
                   )
                 )}
-                <Button className="bg-white rounded-[10px] text-[17px] text-[#4971ff] font-nunito font-bold py-[7px] shadow-standart w-full mb-[80px] sm:mb-[15px] mt-[20px]">
+                <Button onClick={getQuestions} className="bg-white rounded-[10px] text-[17px] text-[#4971ff] font-nunito font-bold py-[7px] shadow-standart w-full mb-[80px] sm:mb-[15px] mt-[20px]">
                   Загрузить еще
                 </Button>
               </>
